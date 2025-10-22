e7434d1605f4fab9f771133e8a5d8425cd2f01bf
Kumar Shan <30646609+kumaruidivloper@users.noreply.github.com>
2025-10-22 11:14:41 +1100

    Create TestApp.swift

---
+++ src/assets/mfe/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Untitled.swift
+//  Test
+//
+//  Created by Kumar Shan on 18/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView() // 👈 This should exist
+        }
+    }
+}
